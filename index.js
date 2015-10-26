59baccf35419572fd10851ef9afa5ad9280f15ca
marrekk <marek.kaminski@gmail.com>
2015-10-26 17:24:25 +0100

    full name replacement + path flattening

--- index.js
+++ index.js
@@ -1,72 +1,81 @@
-var path = require('path')
-, gutil = require('gulp-util')
-, through = require('through2')
-, crypto = require('crypto')
-, fs = require('fs')
-, glob = require('glob');
+var path = require('path'),
+    gutil = require('gulp-util'),
+    through = require('through2'),
+    crypto = require('crypto'),
+    fs = require('fs'),
+    glob = require('glob');
 
-module.exports = function (size, ifile) {
-    size = size | 0;
-    return through.obj(function (file, enc, cb) {
-        if (file.isStream()) {
-            this.emit('error', new gutil.PluginError('gulp-debug', 'Streaming not supported'));
-            return cb();
-        }
+(function() {
+    function calcMd5(file, slice) {
+        var md5 = crypto.createHash('md5');
+        md5.update(file.contents, 'utf8');
 
-        if(!file.contents){
-            return cb();
-        }
+        return slice > 0 ? md5.digest('hex').slice(0, slice) : md5.digest('hex');
+    }
 
-        var d = calcMd5(file, size)
-        , filename = path.basename(file.path)
-        , relativepath = path.relative(file.base ,file.path)
-        , sub_namepath = relativepath.replace(new RegExp(filename) , "").split(path.sep).join('/')
-        , dir;
+    module.exports = function(size, ifile, replaceBasename, flattenPath) {
+        size = size || 0;
 
-        if(file.path[0] == '.'){
-            dir = path.join(file.base, file.path);
-        } else {
-            dir = file.path;
-        }
-        dir = path.dirname(dir);
+        return through.obj(function(file, enc, cb) {
+            if (file.isStream()) {
+                this.emit('error', new gutil.PluginError('gulp-debug', 'Streaming not supported'));
+                return cb();
+            }
 
-        var md5_filename = filename.split('.').map(function(item, i, arr){
-            return i == arr.length-2 ? item + '_'+ d : item;
-        }).join('.');
+            if (!file.contents) {
+                return cb();
+            }
 
-        if(Object.prototype.toString.call(ifile) == "[object Array]"){
-            ifile.forEach(function(i_ifile){
-                i_ifile && glob(i_ifile,function(err, i_files){
-                    if(err) return console.log(err);
-                    i_files.forEach(function(i_ilist){
-                        var result = fs.readFileSync(i_ilist,'utf8').replace(new RegExp(filename), md5_filename);
-                        fs.writeFileSync(i_ilist, result, 'utf8');
-                    })
-                })
-            })
-        }else{
-            ifile && glob(ifile,function(err, files){
-                if(err) return console.log(err);
-                files.forEach(function(ilist){
-                    var result = fs.readFileSync(ilist,'utf8').replace(new RegExp(filename), md5_filename);
-                    fs.writeFileSync(ilist, result, 'utf8');
-                })
-            })
-        }
+            var d = calcMd5(file, size),
+                filename = path.basename(file.path),
+                relativepath = path.relative(file.base, file.path),
+                dir;
 
-        file.path = path.join(dir, md5_filename);
+            if (file.path[0] === '.') {
+                dir = path.join(file.base, file.path);
+            } else {
+                dir = file.path;
+            }
+            dir = path.dirname(dir);
 
-        this.push(file);
-        cb();
-    }, function (cb) {
-        cb();
-    });
-};
+            var getReplacedName = function() {
+                var dotPos = filename.lastIndexOf(".");
+                return d + filename.substr(dotPos);
+            };
 
+            var getName = function() {
+                return filename.split('.').map(function(item, i, arr) {
+                    return i === arr.length - 2 ? item + '_' + d : item;
+                }).join('.');
+            };
 
-function calcMd5(file, slice){
-    var md5 = crypto.createHash('md5');
-    md5.update(file.contents, 'utf8');
+            var md5_filename = replaceBasename ? getReplacedName() : getName();
+            var searched_path = flattenPath ? relativepath.replace(/\\/g, '/') : filename;
 
-    return slice >0 ? md5.digest('hex').slice(0, slice) : md5.digest('hex');
-}
+            var overwriteFileInTpl = function(_ifile) {
+                _ifile && glob(_ifile, function(err, files) {
+                    if (err) {
+                        return console.log(err);
+                    }
+                    files.forEach(function(ilist) {
+                        var result = fs.readFileSync(ilist, 'utf8').replace(new RegExp(searched_path), md5_filename);
+                        fs.writeFileSync(ilist, result, 'utf8');
+                    });
+                });
+            };
+
+            if (Object.prototype.toString.call(ifile) === "[object Array]") {
+                ifile.forEach(overwriteFileInTpl);
+            } else {
+                overwriteFileInTpl(ifile);
+            }
+
+            file.path = path.join(dir, md5_filename);
+
+            this.push(file);
+            cb();
+        }, function(cb) {
+            cb();
+        });
+    };
+})();
